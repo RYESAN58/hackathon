8da2823c72dcc40439eb1a3a1936daabab8fb55b
Verified ✔️ <ryesan58@gmail.com>
2022-06-19 19:39:23 -0400

    Added register form

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
-# from flask_app.models.user import User
+from flask_app.models.user import User
 # from flask_app.models.show import Show
 # from flask_app.models.liked import Liked
 
@@ -11,3 +11,31 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/register')
+def reggie():
+    return render_template("register.html")
+
+
+
+@app.route('/create_user', methods= ['POST'])
+def created():
+
+    if not User.validate(request.form):
+        return redirect('/')
+    data = {
+        'firstname' : request.form['firstname'],
+        'lastname' : request.form['lastname'],
+        'email' : request.form['email'],
+    }
+
+
+    x = {'email':request.form['email']}
+    checker = User.verify_email(x)
+    if checker == False:
+        return redirect('/')
+
+    else:
+        y = User.create(data)
+        flash('Succesfully Added in database', 'success')
+        return redirect('/')
---
+++ flask_app/models/user.py
@@ -0,0 +1,94 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+class User:
+    def __init__(self, db_data):
+        self.id = db_data['id']
+        self.firstname = db_data['firstname']
+        self.lastname = db_data['lastname']
+        self.email = db_data['email']
+        # self.password = db_data['password']
+
+
+
+    @classmethod
+    def create(cls, data):
+        query = "INSERT INTO `hackathon`.`user` (`firstname`, `lastname`, `email`) VALUES (%(firstname)s, %(lastname)s, %(email)s);"
+        return connectToMySQL("black").query_db(query,data)
+
+
+
+    @classmethod
+    def retrieve(cls):
+        query = 'SELECT * FROM user'
+        which = connectToMySQL('hackathon').query_db(query)
+        numbers = []
+        for i in which:
+            numbers.append(cls(i))
+        return numbers
+
+
+    #retriever all specific data
+    @classmethod
+    def retrieve_by(cls, data):
+        query = "SELECT * FROM user WHERE id = %(id)s"
+        return connectToMySQL('black').query_db(query, data)
+
+    @classmethod
+    def verify_email(cls,data):
+        is_valid = True
+        query = 'SELECT * FROM user WHERE email = %(email)s;'
+        result = connectToMySQL("black").query_db(query,data)
+        if len(result)  != 0:
+            flash('This Email is already Taken', 'login')
+            is_valid = False
+        return is_valid
+
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM user WHERE email = %(email)s;"
+        result = connectToMySQL("black").query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    #update data
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE `black`.`user` SET `firstname` = %(firstname)s, `lastname`=%(lastname)s, `email`= %(email)s, `password`= %(password)s WHERE (`id` = %(id)s);"
+        return connectToMySQL('black').query_db(query, data)
+
+    #delete row 
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM `black`.`user` WHERE (`id` = %(id)s);"
+        return connectToMySQL('black').query_db(query, data)
+
+
+    @staticmethod
+    def validate(user):
+        is_valid = True
+        if len(user['firstname']) < 3:
+            flash("firstname must be at least 3 characters.", 'create')
+            is_valid = False
+        if len(user['lastname']) < 3:
+            flash("lastname must be at least 3 characters.", 'create')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", 'create')
+            is_valid = False
+        if len(user['password']) < 7:
+            flash("password must be at least 7 characters", 'create')
+            is_valid = False
+        if user['password'] != user['password2']:
+            flash('Both passwords must match', 'create')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash('password must be 8 characters', 'create')
+            is_valid = False
+        return is_valid
